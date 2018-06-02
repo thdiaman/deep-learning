67e13a0708b847bf2a259e86b73552693e2a52e9
thdiaman <themisdiamantopoulos@hotmail.com>
2018-06-03 00:30:21 +0300

    Add CNN for OCR

---
+++ OCR/testing_cnn.py
@@ -0,0 +1,54 @@
+import numpy as np
+import tkinter as tk
+from PIL import Image, ImageDraw
+from keras.models import load_model
+
+model = load_model('cnn_model.h5')
+
+def evaluate():
+    img = image1.resize((28, 28)).convert('L')
+    pixels = img.load()
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            pixels[i,j] = 255 if pixels[i,j] == 0 else 0
+    X_test = np.array(img)
+    X_test = X_test.reshape(1, 28, 28, 1)
+    predicted_classes = model.predict_classes(X_test, verbose=0)
+    reslabel['text'] = "The drawn number is " + str(predicted_classes[0])
+
+def clear():
+    pixels = image1.load()
+    for i in range(image1.size[0]):
+        for j in range(image1.size[1]):
+            pixels[i,j] = (255, 255,255)
+    cv.delete("all")
+    reslabel['text'] = "Draw a number & press Evaluate"
+
+def paint(event):
+    x1, y1 = (event.x - 10), (event.y - 10)
+    x2, y2 = (event.x + 10), (event.y + 10)
+    cv.create_oval(x1, y1, x2, y2, fill="black",width=4)
+    draw.ellipse([x1, y1, x2, y2],fill="black")
+
+root = tk.Tk()
+root.title("OCR application")
+root.resizable(False, False)
+
+# Create top canvas and image
+cv = tk.Canvas(root, width=280, height=280, bg='white')
+cv.pack()
+image1 = Image.new("RGB", (280, 280), (255, 255, 255))
+draw = ImageDraw.Draw(image1)
+cv.bind("<B1-Motion>", paint)
+
+# Create bottom label and buttons
+bottom = tk.Frame(root)
+bottom.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+reslabel = tk.Label(text = "Draw a number & press Evaluate")
+reslabel.pack(in_=bottom, side=tk.LEFT, fill=tk.Y, expand=True)
+button = tk.Button(text="evaluate", command=evaluate, width=6)
+button.pack(in_=bottom, side=tk.LEFT, fill=tk.Y)
+button = tk.Button(text="clear", command=clear, width=6)
+button.pack(in_=bottom, side=tk.RIGHT, fill=tk.Y)
+
+root.mainloop()
---
+++ OCR/training_cnn.py
@@ -0,0 +1,70 @@
+'''
+Train a convolutional NN (CNN) on the MNIST dataset.
+'''
+
+# Import basic libraries and keras
+import os
+import numpy as np
+
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Convolution2D, MaxPooling2D
+
+import keras
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout
+
+# Read the MNIST data and split to train and test
+f = np.load('mnist.npz')
+x_train, y_train = f['x_train'], f['y_train']
+x_test, y_test = f['x_test'], f['y_test']
+f.close()
+
+# Change depth of image to 1
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+# Change type from int to float and normalize to [0, 1]
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+# Optionally check the number of samples
+#print(x_train.shape[0], 'train samples')
+#print(x_test.shape[0], 'test samples')
+
+# Convert class vectors to binary class matrices (transform the problem to multi-class classification)
+num_classes = 10
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# Check if there is a pre-trained model
+if not os.path.exists('cnn_model.h5'):
+    # Create a neural network with 2 convolutional layers and 2 dense layers
+    model = Sequential()
+    model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(28,28,1)))
+    model.add(Convolution2D(32, 3, 3, activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+    
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
+
+    model.summary()
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # Train the model
+    model.fit(x_train, y_train, batch_size=32, epochs=10, verbose=1, validation_data=(x_test, y_test))
+
+    # Save the model
+    model.save('cnn_model.h5')
+
+else:
+    # Load the model from disk
+    model = load_model('cnn_model.h5')
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
